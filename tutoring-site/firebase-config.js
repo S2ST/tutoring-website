a6a9b6f0e027f52e62aab481e8bbe0cc82973191
Razi Syed <r33syed@uwaterloo.ca>
2022-06-24 00:24:47 -0400

    firebase setup

---
+++ tutoring-site/firebase-config.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAi6CGdNU5X2OruipS56iJqxvvztvDRSgo",
+  authDomain: "website-d9767.firebaseapp.com",
+  projectId: "website-d9767",
+  storageBucket: "website-d9767.appspot.com",
+  messagingSenderId: "702793260382",
+  appId: "1:702793260382:web:12696d89587000c920c3cf",
+  measurementId: "G-HP1NXMGGLJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
