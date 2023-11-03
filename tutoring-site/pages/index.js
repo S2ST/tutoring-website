778845c0f57d7736eae92d5c70c3b90a2398a756
Raihaan Sandhu <63803872+GaminRick7@users.noreply.github.com>
2023-11-03 01:38:27 -0400

    Update index.js

--- tutoring-site/pages/index.js
+++ tutoring-site/pages/index.js
@@ -244,16 +244,16 @@ export default function Home() {
   <div className="mx-auto max-w-7xl px-6 lg:px-8">
     <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
       <div className="mx-auto flex max-w-xs flex-col gap-y-4">
-        <dt className="text-base leading-7 text-gray-600">Transactions every 24 hours</dt>
-        <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">44 million</dd>
+        <dt className="text-base leading-7 text-gray-600">Students helped</dt>
+        <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">2,000+</dd>
       </div>
       <div className="mx-auto flex max-w-xs flex-col gap-y-4">
-        <dt className="text-base leading-7 text-gray-600">Assets under holding</dt>
-        <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">$119 trillion</dd>
+        <dt className="text-base leading-7 text-gray-600">Tutors hired</dt>
+        <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">100+</dd>
       </div>
       <div className="mx-auto flex max-w-xs flex-col gap-y-4">
-        <dt className="text-base leading-7 text-gray-600">New users annually</dt>
-        <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">46,000</dd>
+        <dt className="text-base leading-7 text-gray-600">Paid to tutors</dt>
+        <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">$195,000</dd>
       </div>
     </dl>
   </div>
@@ -267,59 +267,23 @@ export default function Home() {
       <div className="grid mb-8 lg:mb-12 lg:grid-cols-2">
           <figure className="flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-b border-gray-200 md:p-12 lg:border-r">
               <blockquote className="mx-auto mb-8 max-w-2xl text-gray-500">
-                  <h3 className="text-lg font-semibold text-gray-900">Speechless with how easy this was to integrate</h3>
-                  <p className="my-4">I recently got my hands on Flowbite Pro, and holy crap, I&aposm speechless with how easy this was to integrate within my application. Most templates are a pain, code is scattered, and near impossible to theme.</p>
-                  <p className="my-4">Flowbite has code in one place and I&aposm not joking when I say it took me a matter of minutes to copy the code, customise it and integrate within a Laravel + Vue application.</p>
-                  <p className="my-4">If you care for your time, I hands down would go with this.</p>
+                  <h3 className="text-lg font-semibold text-gray-900">Amazing!</h3>
+                  <p className="my-4">Thank you so so much to all the organizers for taking the time for everything. I am in 10th grade and I feel like so many of my queries have been solved. Your courses made me feel motivated in going towards my strengths, my goals, and my passions. Thank you 😊</p>
               </blockquote>
               <figcaption className="flex justify-center items-center space-x-3">
-                  <Image className="w-9 h-9 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png" alt="profile picture"/>
                   <div className="space-y-0.5 font-medium">
-                      <div>Bonnie Green</div>
-                      <div className="text-sm font-light text-gray-500">Parent</div>
+                      <div>Parent</div>
                   </div>
               </figcaption>    
           </figure>
           <figure className="flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-b border-gray-200 md:p-12">
               <blockquote className="mx-auto mb-8 max-w-2xl text-gray-500">
-                  <h3 className="text-lg font-semibold text-gray-900">Solid foundation for any project</h3>
-                  <p className="my-4">"FlowBite provides a robust set of design tokens and components based on the popular Tailwind CSS framework. From the most used UI components like forms and navigation bars to the whole app screens designed both for desktop and mobile, this UI kit provides a solid foundation for any project.</p>
-                  <p className="my-4">Designing with Figma components that can be easily translated to the utility classes of Tailwind CSS is a huge timesaver!"</p>
+                  <h3 className="text-lg font-semibold text-gray-900">Great tutors!</h3>
+                  <p className="my-4">"My daughter loves your courses! She especially likes being taught by tutors that can relate to her, and she’s learned so much from them! Keep up your work! 🙏🙏🙏</p>
               </blockquote>
               <figcaption className="flex justify-center items-center space-x-3">
-                  <Image className="w-9 h-9 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/roberta-casas.png" alt="profile picture"/>
                   <div className="space-y-0.5 font-medium">
-                      <div>Roberta Casas</div>
-                      <div className="text-sm font-light text-gray-500">Professor at University of Ottawa</div>
-                  </div>
-              </figcaption>    
-          </figure>
-          <figure className="flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-b border-gray-200 lg:border-b-0 md:p-12 lg:border-r">
-              <blockquote className="mx-auto mb-8 max-w-2xl text-gray-500">
-                  <h3 className="text-lg font-semibold text-gray-900">Mindblowing workflow and variants</h3>
-                  <p className="my-4">As someone who mainly designs in the browser, I&aposve been a casual user of Figma, but as soon as I saw and started playing with FlowBite my mind was 🤯.</p>
-                  <p className="my-4">Everything is so well structured and simple to use (I&aposve learnt so much about Figma by just using the toolkit).</p>
-                  <p className="my-4">Aesthetically, the well designed components are beautiful and will undoubtedly level up your next application.</p>
-              </blockquote>
-              <figcaption className="flex justify-center items-center space-x-3">
-                  <Image className="w-9 h-9 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="profile picture"/>
-                  <div className="space-y-0.5 font-medium">
-                      <div>Jese Leos</div>
-                      <div className="text-sm font-light text-gray-500">Student</div>
-                  </div>
-              </figcaption>    
-          </figure>
-          <figure className="flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-gray-200 md:p-12">
-              <blockquote className="mx-auto mb-8 max-w-2xl text-gray-500">
-                  <h3 className="text-lg font-semibold text-gray-900">Efficient Collaborating</h3>
-                  <p className="my-4">This is a very complex and beautiful set of elements. Under the hood it comes with the best things from 2 different worlds: Figma and Tailwind.</p>
-                  <p className="my-4">You have many examples that can be used to create a fast prototype for your team.</p>
-              </blockquote>
-              <figcaption className="flex justify-center items-center space-x-3">
-                  <Image className="w-9 h-9 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/joseph-mcfall.png" alt="profile picture"/>
-                  <div className="space-y-0.5 font-medium">
-                      <div>Joseph McFall</div>
-                      <div className="text-sm font-light text-gray-500">Parent</div>
+                      <div>Parent</div>
                   </div>
               </figcaption>    
           </figure>
